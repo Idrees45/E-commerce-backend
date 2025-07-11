0095c88f9145554b1bfc47aa5a9b393091b98f73
Muhammad  Idrees <108980313+Idrees45@users.noreply.github.com>
2025-07-11 21:04:11 +0500

    Update userlogout.js

--- controller/user/userlogout.js
+++ controller/user/userlogout.js
@@ -1,21 +1,46 @@
 
 
-const userlogout= async(req,res)=>{
-    try {
+// const userlogout= async(req,res)=>{
+//     try {
         
-        res.clearCookie("token")
-        res.json({
-            message:"logout successfully",
-            error:false,
-            success:true,
-            data:[]
-            })
-    } catch (error) {
-        res.json({
-            message:error.message||error,
-            error:true,
-            success:false
-            })
-    }
-}
+//         res.clearCookie("token")
+//         res.json({
+//             message:"logout successfully",
+//             error:false,
+//             success:true,
+//             data:[]
+//             })
+//     } catch (error) {
+//         res.json({
+//             message:error.message||error,
+//             error:true,
+//             success:false
+//             })
+//     }
+// }
+// module.exports =userlogout
+
+
+const userlogout = async (req, res) => {
+  try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "None"
+    });
+
+    res.json({
+      message: "Logout successfully",
+      error: false,
+      success: true,
+      data: []
+    });
+  } catch (error) {
+    res.json({
+      message: error.message || error,
+      error: true,
+      success: false
+    });
+  }
+};
 module.exports =userlogout
