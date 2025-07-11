255f9cc910e80bee0598cb2ba971d6046a92dced
Muhammad  Idrees <108980313+Idrees45@users.noreply.github.com>
2025-07-11 20:38:25 +0500

    Update userDetails.js

--- controller/user/userDetails.js
+++ controller/user/userDetails.js
@@ -2,7 +2,7 @@
 const userModel = require("../../models/userModel")
 const userDetails= async(req,res)=>{
 try {
-    // console.log("user_id", req.user.id)
+    console.log("user_id", req.user.id)
        const user= await userModel.findById(req.user.id)
        res.status(200).json({
         data:user,
