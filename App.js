1a1102cdf6b9cabbdb94e25787a99ea7aafdd3c0
Muhammad  Idrees <108980313+Idrees45@users.noreply.github.com>
2025-07-11 22:03:22 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,3 +1,37 @@
+// const express= require("express");
+// const cors= require("cors")
+// require("dotenv").config()
+// const app= express()
+// const bodyParser = require('body-parser');
+// app.use(cors({
+//    origin: 'https://dailyloot.vercel.app',
+//     credentials:true
+// }))
+
+
+// // ✅ Handle preflight requests
+// app.options("*", cors(corsOptions));
+// app.use(express.json())
+// const port = process.env.PORT || 5000;
+// const connectDB=require("./config/db")
+// const router=require("./routes/index")
+// const cookieParser = require('cookie-parser')
+// app.use(cookieParser());
+// // app.use(bodyParser.json({ limit: '50mb' }));
+// // app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+// app.use("/api",router)
+
+// connectDB().then(()=>{
+//     app.listen(port,()=>{
+//         console.log(`server is running on port no ${port}`)
+//         console.log("DATA BASE IS CONNECTED")
+//         })
+// })
+
+
+
+
+
 const express= require("express");
 const cors= require("cors")
 require("dotenv").config()
@@ -7,10 +41,6 @@ app.use(cors({
    origin: 'https://dailyloot.vercel.app',
     credentials:true
 }))
-
-
-// ✅ Handle preflight requests
-app.options("*", cors(corsOptions));
 app.use(express.json())
 const port = process.env.PORT || 5000;
 const connectDB=require("./config/db")
@@ -27,4 +57,3 @@ connectDB().then(()=>{
         console.log("DATA BASE IS CONNECTED")
         })
 })
-
